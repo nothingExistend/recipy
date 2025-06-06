c28e7107d9b504b609d5293fe1dc440cd7261103
nothingExistend <nothing.51208@gmail.com>
2025-06-06 20:33:57 +0200

    changes

--- library.py
+++ library.py
@@ -7,7 +7,7 @@ recipes = {
             "water" : 60,
             "oil" : 10,
         "time" : 30,
-        "instr" : "mix solids thoroughly  |||  mix fluids in separate container  |||  mix mixes  |||  let individual portions stay 15 mins  |||  roll dough out  |||  prepared pizza in oven 200°C, ~15 mins"
+        "instr" : "mix solids thoroughly  |||  mix fluids in separate container  |||  mix mixes  |||  let individual portions stay 15 mins  |||  roll dough out  |||  prepared pizza in oven 200C, ~15 mins"
         }
     }
 }
