263aa5b6ecb85d7a368c612b59da8695281b52f6
nothingExistend <nothing.51208@gmail.com>
2025-06-06 20:32:44 +0200

    changes

--- library.py
+++ library.py
@@ -1,1 +1,13 @@
-recipes = {1,2,3,4}
+recipes = {
+    "pizza" or "1" : {
+        "ingr" : {
+            "flour" : 110,
+            "salt" : 2,
+            "baking powder" : 2.5,
+            "water" : 60,
+            "oil" : 10,
+        "time" : 30,
+        "instr" : "mix solids thoroughly  |||  mix fluids in separate container  |||  mix mixes  |||  let individual portions stay 15 mins  |||  roll dough out  |||  prepared pizza in oven 200°C, ~15 mins"
+        }
+    }
+}
