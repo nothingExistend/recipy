011b36817ee144253142ed06c6e854fcb22e308e
nothingExistend <nothing.51208@gmail.com>
2025-07-28 21:20:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
 
+# URL of the remote Python script
 url = "https://raw.githubusercontent.com/nothingExistend/recipy/refs/heads/main/code.py"
-response = requests.get(url).text
 
-exec(response)
+try:
+    # Download the content of the Python file
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an error for bad status
+
+    code = response.text
+    print("Downloaded code:\n", code[:200], '...\n')  # Show a preview of the code
+
+    # Execute the downloaded code
+    exec(code, {"__name__": "__main__"})
+
+except requests.RequestException as e:
+    print("Failed to download the file:", e)
+except Exception as e:
+    print("Error while executing the code:", e)
